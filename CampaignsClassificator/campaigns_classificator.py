3ae7c557eec1c29206d9996dfb9195c4c953802b
YaninaO <yaninaandreaortiz@gmail.com>
2017-05-08 21:21:56 -0300

    First commit

---
+++ CampaignsClassificator/campaigns_classificator.py
@@ -0,0 +1,16 @@
+# Campaigns classificator file.
+
+#author = yortiz
+
+
+def read_json_file():
+    pass
+
+
+def campaigns_filter():
+    pass
+
+
+if __name__ == '__main__':
+    print("Main File")
+    
